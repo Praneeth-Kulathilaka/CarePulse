6c8dc3466c4eecc3c527934f2532b2a9ce169a66
Praneeth Saranga <praneethskulathilaka@gmail.com>
2024-09-26 04:03:05 +0530

    New Patient form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,16 +5,9 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form"
+import CustomFormField, { FormFieldType } from "../CustomFormField"
+
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -23,43 +16,58 @@ const formSchema = z.object({
 })
 
 const PatientForm = () =>  {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        name: "",
+        email: "",
+        phone: ""
+        },
+    })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-8">
+            <section className="mb-12 space-y-4">
+                <h1 className="header">Hi there 👋</h1>
+                <p className="text-dark-700">Get started with appointments.</p>
+            </section>
+            <CustomFormField
+                fieldType={FormFieldType.INPUT}
+                control={form.control}
+                name="name"
+                label="Full name"
+                placeholder="John Doe"
+                iconSrc="/assets/icons/user.svg"
+                iconAlt="user"
+            />
+            <CustomFormField
+                fieldType={FormFieldType.INPUT}
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="johndoe@gmail.com"
+                iconSrc="/assets/icons/email.svg"
+                iconAlt="email"
+            />
+            <CustomFormField
+                fieldType={FormFieldType.PHONE_INPUT}
+                control={form.control}
+                name="phone"
+                label="Phone number"
+                placeholder="(555) 123-4567"
+            />
+            <Button type="submit">Submit</Button>
+        </form>
+        </Form>
+    )
 }
 
 
